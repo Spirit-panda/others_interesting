52ef1d15de1ecb01ddb8374963b1b107659fd71e
fengyi <125357423+Spirit-panda@users.noreply.github.com>
2026-05-26 11:42:10 +0800

    Create Theday.f90

---
+++ Theday.f90
@@ -0,0 +1,17 @@
+program happy
+
+      implicit none
+
+      character*100::answer, thing, eating, studying
+
+     print*, "Are you happpy today?"
+     read*, answer
+     print*, answer
+     print*, "Have you enjoyed your time?"
+     read*, thing
+     print*, thing
+     print*, "How to finish your work effeciently?"
+     read*, studying
+     print*, studying
+
+end program happy
