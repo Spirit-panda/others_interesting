ad99c23410f6041784f8c9549670c4bda3a99a73
fengyi <125357423+Spirit-panda@users.noreply.github.com>
2026-05-30 12:57:17 +0800

    Update Theday.f90

--- Theday.f90
+++ Theday.f90
@@ -5,13 +5,14 @@ program happy
       character*100::answer, thing, eating, studying
 
      print*, "Are you happpy today?"
-     read(*, 'A'), answer
+     read(*, '(A)') answer
      print*, answer
      print*, "Have you enjoyed your time?"
-     read(*, 'A'), thing
+     read(*, '(A)') thing
      print*, thing
      print*, "How to finish your work effeciently?"
-     read(*, 'A'), studying
+     read(*, '(A)') studying
      print*, studying
 
 end program happy
+
